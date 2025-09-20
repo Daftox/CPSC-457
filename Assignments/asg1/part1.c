190112fbf67177e6f0eb91fd65f40d30cf8915be
Timothée Chuat <timothee.chuat@edu.hefr.ch>
2025-09-19 23:40:16 -0600

    enable printing when a child searches its row

--- Assignments/asg1/part1.c
+++ Assignments/asg1/part1.c
@@ -6,74 +6,90 @@
 #define ROWS 100
 #define COLUMNS 1000
 
-int main() {
+int main()
+{
 
-    int fd[2];   // fd[0] = read end, fd[1] = write end
-    if (pipe(fd) == -1) {
+    int fd[2]; // fd[0] = read end, fd[1] = write end
+    if (pipe(fd) == -1)
+    {
         perror("pipe failed");
         _exit(1);
     }
 
-    char line[2*COLUMNS+1]; //little extra room to handle EOL
+    char line[2 * COLUMNS + 1]; // little extra room to handle EOL
 
     // PARENT - INIT ------------------------------------
-    for (int i = 0; i < ROWS; i++) {
+    for (int i = 0; i < ROWS; i++)
+    {
 
-        if (fgets(line, sizeof(line), stdin) == NULL) {
+        if (fgets(line, sizeof(line), stdin) == NULL)
+        {
             break;
         }
 
         pid_t pid = fork();
-        if (pid < 0) {
+        if (pid < 0)
+        {
             perror("fork failed");
             return 1;
         }
 
         // CHILDS ----------------------------------------
-        if (pid == 0) {
+        if (pid == 0)
+        {
 
             close(fd[0]); // close unused read end
-            //printf("Child %d (PID %d): Searching row\n", i, getpid(), i);
-
-            char *p = line;    // pointer to start of line
+            /* note that ther is acontradictory expectations regarding this print
+            between the README file in Inputs folder and the assignment description
+            The line is printed to respect the "Assignment 1 - Fork.pdf" file
+            */
+            printf("Child %d (PID %d): Searching row\n", i, getpid(), i);
+            char *p = line; // pointer to start of line
             int col = 0;
 
-            while (col < COLUMNS && *p != '\0' && *p != '\n') {
-                if (*p == '1') {
-                    if (write(fd[1], &col, sizeof col) == -1) {
+            while (col < COLUMNS && *p != '\0' && *p != '\n')
+            {
+                if (*p == '1')
+                {
+                    if (write(fd[1], &col, sizeof col) == -1)
+                    {
                         perror("write pipe failed");
                         _exit(1);
                     }
                     close(fd[1]);
-                    _exit(0);  // treasure found
+                    _exit(0); // treasure found
                 }
 
-                if (*p != '0'){
+                if (*p != '0')
+                {
                     printf("unexpected value %d in the provided text file", *p);
                     _exit(1);
                 }
                 col++;
-                p+=2;  // move pointer forward, ignoring the space
+                p += 2; // move pointer forward, ignoring the space
             }
 
             close(fd[1]); // close unused write end
             _exit(1);
-        }// ----------------------------------------------
+        } // ----------------------------------------------
     }
 
     // PARENT - CLOSE ------------------------------------
     close(fd[1]); // close unused write end
 
     int treasure_col;
-    if (read(fd[0], &treasure_col, sizeof(treasure_col)) == -1) {
+    if (read(fd[0], &treasure_col, sizeof(treasure_col)) == -1)
+    {
         perror("read pipe failed");
         _exit(1);
     }
 
-    for (int i = 0; i < ROWS; i++) {
+    for (int i = 0; i < ROWS; i++)
+    {
         int status;
         pid_t pid_child = wait(&status);
-        if (WEXITSTATUS(status) == 0){
+        if (WEXITSTATUS(status) == 0)
+        {
             printf("Parent: The treasure was found by child with PID %d at row %d, column %d\n", pid_child, i, treasure_col);
             close(fd[0]); // close unused read end
             _exit(0);
